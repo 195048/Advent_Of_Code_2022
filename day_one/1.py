c689b73ea9d5580bb46f87bc065bfaff8efe1e22
RayaneS <78797911+195048@users.noreply.github.com>
2022-12-07 12:32:42 +0100

    day one problem solved

---
+++ day_one/1.py
@@ -0,0 +1,18 @@
+
+lines = []
+elves = []
+calories = []
+
+with open("C:\\Users\\saidi\\Desktop\\Advent_Of_Code_2022\\day_one\\1.txt", "r") as input:
+    for line in input:
+        lines.append(line.strip())
+
+elf = []
+for elem in lines : 
+    if elem == '': 
+        elves.append(sum(elf))
+        elf.clear()
+    else :
+        elf.append(int(elem))
+    
+print(max(elves))
